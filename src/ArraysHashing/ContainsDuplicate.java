310bb1980daf2b671602242ee803f3202fa7dfae
Jdollx <jenniferdolldev@gmail.com>
2024-09-09 15:31:08 -0400

    completed DUPLICATE INTEGER

--- src/ArraysHashing/ContainsDuplicate.java
+++ src/ArraysHashing/ContainsDuplicate.java
@@ -1,1 +1,17 @@
+// DUPLICATE INTEGER
 // Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+// Easy - 9/9/2024
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // use two for loops to compare against each other
+        // Big O(2N)
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++)
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+        }
+        return false;
+    }
+}
