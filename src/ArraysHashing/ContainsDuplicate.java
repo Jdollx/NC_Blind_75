12a97f0c7b0359840a2d79d2c6cf2f18d711a02e
Jdollx <jenniferdolldev@gmail.com>
2024-09-09 15:38:54 -0400

    completed DUPLICATE INTEGER

--- src/ArraysHashing/ContainsDuplicate.java
+++ src/ArraysHashing/ContainsDuplicate.java
@@ -4,7 +4,8 @@
 
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        // use two for loops to compare against each other
+        // could use hashmap, two pointers, or for loops
+        // use two for loops to compare against each other since not concerned with storage
         // Big O(2N)
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++)
